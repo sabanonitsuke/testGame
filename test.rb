774d0e688e7d117a0f47b89e9af4d1cf66a89995
sabanonitsuke <sabakan.nari@gmail.com>
2023-05-10 23:51:37 +0900

    tutorialクラスの動作追加

--- test.rb
+++ test.rb
@@ -1,17 +1,39 @@
 require 'gosu'
+require './player.rb'
 
 class Tutorial < Gosu::Window
   def initialize
     super 640, 480
     self.caption = "Tutorial Game"
     @bg = Gosu::Image.new("app/assets/images/food_kandume_saba.png", :tileable => true)
+    @player = Player.new
+    @player.warp(320, 240)
   end
 
   def update
+    if Gosu::button_down?(Gosu::KB_LEFT) || Gosu::button_down?(Gosu::GP_LEFT)
+      @player.turn_left
+    end
+    if Gosu::button_down?(Gosu::KB_RIGHT) || Gosu::button_down?(Gosu::GP_RIGHT)
+      @player.turn_right
+    end
+    if Gosu::button_down?(Gosu::KB_UP) || Gosu::button_down?(Gosu::GP_BUTTON_0)
+      @player.accelerate
+    end
+    @player.move
   end
 
   def draw
     @bg.draw(0, 0, 0)
+    @player.draw
+  end
+
+  def button_down(id)
+    if id == Gosu::KB_ESCAPE
+      close
+    else
+      super
+    end
   end
 end
 
