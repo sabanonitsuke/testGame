289f9108314e050ee24abf726ec3c3fe69006e59
sabanonitsuke <sabakan.nari@gmail.com>
2023-05-10 23:29:29 +0900

    画像を表示させる

--- test.rb
+++ test.rb
@@ -4,12 +4,14 @@ class Tutorial < Gosu::Window
   def initialize
     super 640, 480
     self.caption = "Tutorial Game"
+    @bg = Gosu::Image.new("app/assets/images/food_kandume_saba.png", :tileable => true)
   end
 
   def update
   end
 
   def draw
+    @bg.draw(0, 0, 0)
   end
 end
 
