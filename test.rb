300aa04ba0088d45faf7c08d55aa2e7bab72ef7c
sabanonitsuke <sabakan.nari@gmail.com>
2023-05-10 23:18:44 +0900

    とりあえず画面表示のコード

---
+++ test.rb
@@ -0,0 +1,16 @@
+require 'gosu'
+
+class Tutorial < Gosu::Window
+  def initialize
+    super 640, 480
+    self.caption = "Tutorial Game"
+  end
+
+  def update
+  end
+
+  def draw
+  end
+end
+
+Tutorial.new.show
